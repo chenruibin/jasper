7b31f5f4a0e82a006db55d82c831e5a8153a258f
ruibin <dev.ruibin.chen@gmail.com>
2015-11-15 09:31:26 +0800

    Update 2015-11-15T01:30:55.317Z

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gp_deploy = require('gulp-gh-pages');
+
+var options = {
+}
+
+gulp.task('deploy', function () {
+    return gulp.src('./_site/**/*')
+        .pipe(gp_deploy(options));
+});
+
+gulp.task('default',['deploy'] ,function () {
+    console.log('default');
+});
