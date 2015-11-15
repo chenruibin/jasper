dc6536d95c67ee95a77c96eb4614fc7b8a0efe3c
ruibin <dev.ruibin.chen@gmail.com>
2015-11-15 10:36:54 +0800

    Update 2015-11-15T02:36:42.139Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gp_deploy = require('gulp-gh-pages');
 var gp_copy = require('gulp-copy');
+var gulp_sequence = require('gulp-sequence');
 
 var options = {
 }
@@ -11,10 +12,11 @@ gulp.task('copy', function (){
 });
 
 gulp.task('deploy', function () {
-  return gulp.src('./_site/**/*')
+  return gulp.src(['./_site/**/*', '!./node_modules/**'])
   .pipe(gp_deploy(options));
 });
 
-gulp.task('default',['copy', 'deploy'] ,function () {
+gulp.task('default', function(cb) {
   console.log('default');
+  gulp_sequence('copy', 'deploy', cb);
 });
